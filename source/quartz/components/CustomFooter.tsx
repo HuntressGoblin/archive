e52439642eead6efeb6bfa633edc109b5d3f9cef
HuntressGoblin <canaries.aria@gmail.com>
2026-03-11 02:00:36 -0500

    vault backup: 2026-03-11 02:00:36

--- source/quartz/components/CustomFooter.tsx
+++ source/quartz/components/CustomFooter.tsx
@@ -6,10 +6,10 @@ export default (() => {
 
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>© 2020 - {year} This is a placeholder</p>
+        <p>© 2023 - {year} Headhuntress. All original content on this site is the property of the author unless otherwise noted.</p>
         <ul>
           <li>
-            <a href="https://lore.enutheta.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://archive.goblindnd.com/" target="_blank" rel="noopener noreferrer">
               Another placeholder. 
             </a>
           </li>
